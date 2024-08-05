1ba5ef766e9b2866ec844d52dd2ed6502b88d5ca
Harsh <harshb@digitsoftsol.com>
2024-08-05 17:52:01 +0530

    Made compatible for php: ^8.1

---
+++ Dss/AdminPaymentMethod/Model/AdminPaymentMethod.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Digit Software Solutions.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  Dss
+ * @package   Dss_AdminPaymentMethod
+ * @author    Extension Team
+ * @copyright Copyright (c) 2024 Digit Software Solutions. ( https://digitsoftsol.com )
+ */
+declare(strict_types=1);
+
+namespace Dss\AdminPaymentMethod\Model;
+
+class AdminPaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * Payment code
+     *
+     * @var string|bool
+     */
+    public const CODE = 'adminpaymentmethod';
+
+    /**
+     * @var string
+     */
+    protected $_code = self::CODE;
+
+    /**
+     * @var bool
+     */
+    protected $_isOffline = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseCheckout = false;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseInternal = true;
+
+    /**
+     * Get preselect option from config
+     *
+     * @return string
+     */
+    public function getDataPreSelect(): string
+    {
+        return $this->getConfigData('preselect');
+    }
+
+    /**
+     * Get Auto Create Invoice option from config
+     *
+     * @return bool
+     */
+    public function getDataAutoCreateInvoice(): bool
+    {
+        return $this->getConfigData('createinvoice');
+    }
+}
---
+++ Dss/AdminPaymentMethod/Plugin/PreSelect.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Digit Software Solutions.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category  Dss
+ * @package   Dss_AdminPaymentMethod
+ * @author    Extension Team
+ * @copyright Copyright (c) 2024 Digit Software Solutions. ( https://digitsoftsol.com )
+ */
+declare(strict_types=1);
+
+namespace Dss\AdminPaymentMethod\Plugin;
+
+use Dss\AdminPaymentMethod\Model\AdminPaymentMethod;
+use Magento\Sales\Block\Adminhtml\Order\Create\Billing\Method\Form;
+
+class PreSelect
+{
+    /**
+     * PreSelect constructor
+     *
+     * @param \Dss\AdminPaymentMethod\Model\AdminPaymentMethod $model
+     */
+    public function __construct(
+        private AdminPaymentMethod $model
+    ) {}
+
+    /**
+     * After get select method code
+     *
+     * @param \Magento\Sales\Block\Adminhtml\Order\Create\Billing\Method\Form $block
+     * @param string $result
+     * @return bool|string
+     */
+    public function afterGetSelectedMethodCode(
+        Form $block,
+        $result
+    ) {
+        if ($result && $result != 'free') {
+            return $result;
+        }
+
+        $data = $this->model->getDataPreSelect();
+        if ($data) {
+            return AdminPaymentMethod::CODE;
+        }
+        return false;
+    }
+}
